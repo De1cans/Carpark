c942c7b91f2d84efda71f7534629e1cdb3bbcf40
De1cans <15901@coderacademy.edu.au>
2024-09-14 11:34:40 +1000

    Implimented park car funciton

--- functions/carpark_functions.py
+++ functions/carpark_functions.py
@@ -1,11 +1,12 @@
 from classes.parking_slot import ParkingSlot
+from classes.car import Car
 
 def add_slot(carpark):
     # take input the id for the parking slot
     slot_id = input("Enter the id for the parking slot: ")
     # Create an instance of the parking slot
     parking_slot = ParkingSlot(slot_id)
-    # add the slot into the carpark
+    # add that slot to the carpark
     carpark.add_slot(parking_slot)
     print("Parking Slot Added\n")
 
@@ -20,10 +21,28 @@ def delete_slot(carpark):
 
 def list_slots(carpark):
     all_slots = carpark.get_slots()
-    print("\nListing slots... ")
+    print("\nListing slots...")
     if not all_slots:
         print("No slots found.")
     for slot in all_slots:
         print(slot)
-
     print("\n")
+
+def park_car(carpark):
+    # take input the rego of the car
+    reg_no = input("Enter the registration number of the car: ")
+    # if check to see if the car already exists
+    # take input the id of the parking slot
+    slot_id = input("Enter the id of the parking slot: ")
+    # find the parking slot
+    slot_to_park = carpark.find_slot(slot_id)
+    if slot_to_park:
+        # create an instance of the car with rego
+        car_to_park = Car(reg_no)
+        # add the car to the parking slot
+        if slot_to_park.add_car(car_to_park):
+            print("Car parked successfully\n")
+        else:
+            print("Car already parked in the slot\n")
+    else:
+        print("Slot with that id does not exist\n")
